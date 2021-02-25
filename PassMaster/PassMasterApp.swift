e40bcc8f12267e2526fa72737df3349bf8b6f585
Plus1XP <evldev@protonmail.ch>
2021-02-25 13:15:14 +0000

    Release: Basic Project

---
+++ PassMaster/PassMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassMasterApp.swift
+//  PassMaster
+//
+//  Created by aleuts on 25/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct PassMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
