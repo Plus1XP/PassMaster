8ccfaf6e6d2aeff35e67e14d602473b8da30c412
Plus1XP <evldev@protonmail.ch>
2021-03-03 21:11:10 +0000

    Refactor: Remove Picker for Account Selector

--- PassMaster/Views/SelectionView.swift
+++ PassMaster/Views/SelectionView.swift
@@ -11,16 +11,59 @@ struct SelectionView: View {
     
     @StateObject private var model = AccountProcessor()
     
-    @State private var selectedLogin: PasswordModel = PasswordModel(id: 1, name: "Example", userName: "JT@Example.com", password: "M0th3rl4nd!", uRL: "www.example.com", notes: "In Russia password saves you!")
+    @State private var selectedLogin: PasswordModel = PasswordModel(id: 0, name: "Example", userName: "JT@Example.com", password: "M0th3rl4nd!", uRL: "www.example.com", notes: "In Russia password saves you!")
     
-    @State private var selectedCard: CardModel?
+    @State private var selectedCard: CardModel = CardModel(id: 0, name: "Example", number: 4090123456789090, start: "1 JAN", end: "31 DEC", cvv: 321, note: "Petty Cash")
     
-    @State private var selectedNote: NoteModel?
+    @State private var selectedNote: NoteModel = NoteModel(id: 0, title: "Example", body: "Example Text")
     
     @State private var canShowDetailsView = false
     
     var body: some View {
         NavigationView {
+            Form{
+                Section(header: Text("Logins & Passwords")){
+                    
+                    List(model.Passwords, id: \.self) { selectedLogin in
+                        Button(action: {
+                            self.selectedLogin = selectedLogin
+                            self.canShowDetailsView.toggle()
+                        }, label: {
+                            Text(selectedLogin.name)
+                        }).sheet(isPresented: $canShowDetailsView) {
+                            PasswordDetailsView(selection: $selectedLogin)
+                        }
+                    }
+                }
+                Section(header: Text("Cards")){
+                    
+                    List(model.Cards, id: \.self) { selectedCard in
+                        Button(action: {
+                            self.selectedCard = selectedCard
+                            self.canShowDetailsView.toggle()
+                        }, label: {
+                            Text(selectedCard.name)
+                        }).sheet(isPresented: $canShowDetailsView) {
+                            CardDetailsView(selection: $selectedCard)
+                        }
+                    }
+                }
+                Section(header: Text("Notes")){
+                    
+                    List(model.Notes, id: \.self) { selectedNote in
+                        Button(action: {
+                            self.selectedNote = selectedNote
+                            self.canShowDetailsView.toggle()
+                        }, label: {
+                            Text(selectedNote.title)
+                        }).sheet(isPresented: $canShowDetailsView) {
+                            NoteDetailsView(selection: $selectedNote)
+                        }
+                    }
+                }
+            }.navigationBarTitle(Text("PassMaster"))
+            
+/*
             Form{
                 Section{
                     Picker("Select a login", selection: $selectedLogin) {
@@ -65,6 +108,7 @@ struct SelectionView: View {
                     }
                 }
             }.navigationBarTitle(Text("PassMaster"))
+ */
         }
     }
 }
