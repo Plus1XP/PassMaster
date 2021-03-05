7dd6d0b5de2ada18d2673ccf0c54172b51cb92ab
Plus1XP <evldev@protonmail.ch>
2021-03-05 20:47:27 +0000

    Feat: Add AccountToolBar to SelectionView

--- PassMaster/Views/SelectionView.swift
+++ PassMaster/Views/SelectionView.swift
@@ -11,104 +11,126 @@ struct SelectionView: View {
     
     @StateObject private var model = AccountProcessor()
     
-    @State private var selectedLogin: PasswordModel = PasswordModel(id: 0, name: "Example", userName: "JT@Example.com", password: "M0th3rl4nd!", uRL: "www.example.com", notes: "In Russia password saves you!")
+    @State private var selectedLogin: PasswordModel = PasswordModel(id: 0, AccountType: AccountType.Password, name: "Example", userName: "JT@Example.com", password: "M0th3rl4nd!", uRL: "www.example.com", notes: "In Russia password saves you!")
     
-    @State private var selectedCard: CardModel = CardModel(id: 0, name: "Example", number: 4090123456789090, start: "1 JAN", end: "31 DEC", cvv: 321, note: "Petty Cash")
+    @State private var selectedCard: CardModel = CardModel(id: 0, AccountType: AccountType.Card, name: "Example", number: 4090123456789090, start: "1 JAN", end: "31 DEC", cvv: 321, note: "Petty Cash")
     
-    @State private var selectedNote: NoteModel = NoteModel(id: 0, title: "Example", body: "Example Text")
+    @State private var selectedNote: NoteModel = NoteModel(id: 0, AccountType: AccountType.Note, title: "Example", body: "Example Text")
+    
+    @State private var selectedAccount: AccountType = AccountType.Password
     
     @State private var canShowDetailsView = false
+    
+    @State private var canShowCreationView = false
     
     var body: some View {
         NavigationView {
             Form{
                 Section(header: Text("Logins & Passwords")){
-                    
                     List(model.Passwords, id: \.self) { selectedLogin in
-                        Button(action: {
-                            self.selectedLogin = selectedLogin
-                            self.canShowDetailsView.toggle()
-                        }, label: {
-                            Text(selectedLogin.name)
-                        }).sheet(isPresented: $canShowDetailsView) {
-                            PasswordDetailsView(selection: $selectedLogin)
+                        Button(
+                            action: {
+                                self.selectedLogin = selectedLogin
+                                self.canShowDetailsView.toggle()
+                            },
+                            label: {
+                                HStack {
+                                    Text("L")
+                                        .frame(width: 50, height: 50)
+                                        .background(Color.white)
+                                        .cornerRadius(10)
+                                        .padding(10)
+                                    VStack(alignment: .leading) {
+                                        Text(selectedLogin.name)
+                                            .foregroundColor(Color.white)
+                                        Text(selectedLogin.userName)
+                                            .foregroundColor(Color.white)
+                                            .font(.footnote)
+                                    }
+                                }
+                            }).sheet(isPresented: $canShowDetailsView) {
+                                PasswordDetailsView(selection: $selectedLogin)
                         }
                     }
                 }
                 Section(header: Text("Cards")){
-                    
                     List(model.Cards, id: \.self) { selectedCard in
-                        Button(action: {
-                            self.selectedCard = selectedCard
-                            self.canShowDetailsView.toggle()
-                        }, label: {
-                            Text(selectedCard.name)
-                        }).sheet(isPresented: $canShowDetailsView) {
-                            CardDetailsView(selection: $selectedCard)
-                        }
+                        Button(
+                            action: {
+                                self.selectedCard = selectedCard
+                                self.canShowDetailsView.toggle()
+                            },
+                            label: {
+                                HStack {
+                                    Text("C")
+                                        .frame(width: 50, height: 50)
+                                        .background(Color.white)
+                                        .cornerRadius(10)
+                                        .padding(10)
+                                    VStack(alignment: .leading) {
+                                        Text(selectedCard.name)
+                                            .foregroundColor(Color.white)
+                                        Text(String(selectedCard.number))
+                                            .foregroundColor(Color.white)
+                                            .font(.footnote) }
+                                }
+                            }).sheet(isPresented: $canShowDetailsView) {
+                                CardDetailsView(selection: $selectedCard)
+                            }
                     }
                 }
                 Section(header: Text("Notes")){
-                    
                     List(model.Notes, id: \.self) { selectedNote in
-                        Button(action: {
-                            self.selectedNote = selectedNote
-                            self.canShowDetailsView.toggle()
-                        }, label: {
-                            Text(selectedNote.title)
-                        }).sheet(isPresented: $canShowDetailsView) {
-                            NoteDetailsView(selection: $selectedNote)
-                        }
+                        Button(
+                            action: {
+                                self.selectedNote = selectedNote
+                                self.canShowDetailsView.toggle()
+                            },
+                            label: {
+                                HStack {
+                                    Text("N")
+                                        .frame(width: 50, height: 50)
+                                        .background(Color.white)
+                                        .cornerRadius(10)
+                                        .padding(10)
+                                    VStack(alignment: .leading) {
+                                        Text(selectedNote.title)
+                                            .foregroundColor(Color.white)
+                                        Text(selectedNote.body)
+                                            .foregroundColor(Color.white)
+                                            .font(.footnote) }
+                                }
+                            }).sheet(isPresented: $canShowDetailsView) {
+                                NoteDetailsView(selection: $selectedNote)
+                            }
                     }
                 }
+                
+                
+                
             }.navigationBarTitle(Text("PassMaster"))
-            
-/*
-            Form{
-                Section{
-                    Picker("Select a login", selection: $selectedLogin) {
-                        ForEach(model.Passwords, id: \.self) { password in
-                            Button(action: {
-                                self.selectedLogin = password
-                                self.canShowDetailsView.toggle()
-                            }, label: {
-                                Text(password.name)
-                            }).sheet(isPresented: $canShowDetailsView) {
-                                DetailsView(selection: $selectedLogin)
-                            }
+            .navigationBarItems(trailing:
+                                    Button(action: {
+                                            self.canShowCreationView.toggle()}) {
+                                    Label("Add", systemImage: "plus.circle")
+                                }).sheet(isPresented: $canShowCreationView) {
+                                    CreationView()
+                                }
+            .toolbar {
+                ToolbarItem(placement: .bottomBar) {
+                    Picker("Select Account", selection: $selectedAccount) {
+                        ForEach(model.Accounts, id: \.self) { account in
+                            Button(
+                                action: {
+                                    self.selectedAccount = account
+                                },
+                                label: {
+                                    Text(account.rawValue)
+                                })
                         }
-                    }
+                    }.pickerStyle(SegmentedPickerStyle())
                 }
-                Section{
-                    Picker("Select a card", selection: $selectedCard) {
-                        ForEach(model.Cards, id: \.self) { card in
-                            Button(action: {
-                                self.selectedCard = card
-                                self.canShowDetailsView.toggle()
-                            }, label: {
-                                Text(card.name)
-                            }).sheet(isPresented: $canShowDetailsView) {
-                                //DetailsView(selection: $selectedCard)
-                            }
-                        }
-                    }
-                }
-                Section{
-                    Picker("Select a note", selection: $selectedNote) {
-                        ForEach(model.Notes, id: \.self) { note in
-                            Button(action: {
-                                self.selectedNote = note
-                                self.canShowDetailsView.toggle()
-                            }, label: {
-                                Text(note.title)
-                            }).sheet(isPresented: $canShowDetailsView) {
-                                //DetailsView(selection: $selectedNote)
-                            }
-                        }
-                    }
-                }
-            }.navigationBarTitle(Text("PassMaster"))
- */
+            }
         }
     }
 }
