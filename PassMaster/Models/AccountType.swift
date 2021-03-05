905e3396c3453134ce016b04c43750533102b52e
Plus1XP <evldev@protonmail.ch>
2021-03-05 20:38:48 +0000

    Feat: Add AccountType Enum

---
+++ PassMaster/Models/AccountType.swift
@@ -0,0 +1,14 @@
+//
+//  AccountType.swift
+//  PassMaster
+//
+//  Created by aleuts on 05/03/2021.
+//
+
+import Foundation
+
+enum AccountType: String {
+    case Password = "Password"
+    case Card = "Card"
+    case Note = "Note"
+}
