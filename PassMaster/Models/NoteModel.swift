49c02f28c5fd3ad98c0e1519bc75c586ae698cdb
Plus1XP <evldev@protonmail.ch>
2021-03-05 20:40:35 +0000

    Add AccountType Enum to NoteModel

--- PassMaster/Models/NoteModel.swift
+++ PassMaster/Models/NoteModel.swift
@@ -9,11 +9,13 @@ import Foundation
 
 struct NoteModel: Hashable {
     var id: Int
+    var AccountType: AccountType
     var title: String
     var body: String
     
     func hash(into hasher: inout Hasher) {
         hasher.combine(id)
+        hasher.combine(AccountType)
         hasher.combine(title)
         hasher.combine(body)
     }
