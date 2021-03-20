17e419ee0e091f5cd4217fdd80794086bfccedb0
Plus1XP <evldev@protonmail.ch>
2021-03-20 20:13:47 +0000

    Feat: Add Equals function to compare Card models

--- PassMaster/Models/CardModel.swift
+++ PassMaster/Models/CardModel.swift
@@ -11,14 +11,14 @@ struct CardModel: Hashable {
     var id: Int
     var AccountType: AccountType
     var name: String
-    var number: Int
+    var number: String
     var start: String?
     var end: String
-    var cvv: Int
+    var cvv: String
     var note: String?
     
-    static let `mock` = Self(id: 0, AccountType: .Card, name: "example", number: 5105105105105100, start: "JAN 21", end: "DEC 24", cvv: 123, note: "Petty Cash")
-    static let `empty` = Self(id: 0, AccountType: .Card, name: "", number: 0, start: "", end: "", cvv: 0, note: "")
+    static let `mock` = Self(id: 0, AccountType: .Card, name: "example", number: "5105105105105100", start: "JAN 21", end: "DEC 24", cvv: "123", note: "Petty Cash")
+    static let `empty` = Self(id: 0, AccountType: .Card, name: "", number: "", start: "", end: "", cvv: "", note: "")
     
     func hash(into hasher: inout Hasher) {
         hasher.combine(id)
@@ -29,5 +29,17 @@ struct CardModel: Hashable {
         hasher.combine(end)
         hasher.combine(cvv)
         hasher.combine(note)
+    }
+    
+    func equals(_ CompareTo: CardModel) -> Bool {
+        return
+            self.id == CompareTo.id &&
+            self.AccountType == CompareTo.AccountType &&
+            self.name == CompareTo.name &&
+            self.number == CompareTo.number &&
+            self.start == CompareTo.start &&
+            self.end == CompareTo.end &&
+            self.cvv == CompareTo.cvv &&
+            self.note == CompareTo.note
     }
 }
