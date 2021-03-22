98e6a0e3dad7319fa50f5cc9788d9df578a23094
Plus1XP <evldev@protonmail.ch>
2021-03-22 19:25:06 +0000

    Refactor: Replace Start & End Date with Start Month/Year & End Month/Year

--- PassMaster/Models/CardModel.swift
+++ PassMaster/Models/CardModel.swift
@@ -12,21 +12,25 @@ struct CardModel: Hashable {
     var AccountType: AccountType
     var name: String
     var number: String
-    var start: String?
-    var end: String
+    var startMonth: String?
+    var startYear: String?
+    var endMonth: String
+    var endYear: String
     var cvv: String
     var note: String?
     
-    static let `mock` = Self(id: 0, AccountType: .Card, name: "example", number: "5105105105105100", start: "JAN 21", end: "DEC 24", cvv: "123", note: "Petty Cash")
-    static let `empty` = Self(id: 0, AccountType: .Card, name: "", number: "", start: "", end: "", cvv: "", note: "")
+    static let `mock` = Self(id: 0, AccountType: .Card, name: "example", number: "5105105105105100", startMonth: "Jan", startYear: "2021", endMonth: "DEC", endYear: "2024", cvv: "123", note: "Petty Cash")
+    static let `empty` = Self(id: 0, AccountType: .Card, name: "", number: "", startMonth: "", startYear: "", endMonth: "", endYear: "", cvv: "", note: "")
     
     func hash(into hasher: inout Hasher) {
         hasher.combine(id)
         hasher.combine(AccountType)
         hasher.combine(name)
         hasher.combine(number)
-        hasher.combine(start)
-        hasher.combine(end)
+        hasher.combine(startMonth)
+        hasher.combine(startYear)
+        hasher.combine(endMonth)
+        hasher.combine(endYear)
         hasher.combine(cvv)
         hasher.combine(note)
     }
@@ -37,8 +41,10 @@ struct CardModel: Hashable {
             self.AccountType == CompareTo.AccountType &&
             self.name == CompareTo.name &&
             self.number == CompareTo.number &&
-            self.start == CompareTo.start &&
-            self.end == CompareTo.end &&
+            self.startMonth == CompareTo.startMonth &&
+            self.startYear == CompareTo.startYear &&
+            self.endMonth == CompareTo.endMonth &&
+            self.endYear == CompareTo.endYear &&
             self.cvv == CompareTo.cvv &&
             self.note == CompareTo.note
     }
