c77af0c149cef2d483068e9c9d265555af233a04
Plus1XP <evldev@protonmail.ch>
2021-03-27 02:19:00 +0000

    Feat: Add Codable Protocol to PasswordModel

--- PassMaster/Models/PasswordModel.swift
+++ PassMaster/Models/PasswordModel.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct PasswordModel: Identifiable, Hashable {
+struct PasswordModel: Identifiable, Hashable, Codable {
     var id : Int
     var AccountType: AccountType
     var name : String
